d0601e492cc9e7d4554ca7ba247c14cbbf213e02
Jiahe Lin <1375441611@qq.com>
2026-09-17 18:29:52 -0500

    feat: load model, prompt and get response

--- MP1/model_prompting.py
+++ MP1/model_prompting.py
@@ -16,14 +16,29 @@ def prompt_model(dataset, model_name = "deepseek-ai/deepseek-coder-6.7b-base", q
     print(f"Working with {model_name} quantization {quantization}...")
     
     # TODO: download the model
-    
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
   
     if quantization:
         # TODO: load the model with quantization
-        
+        model = AutoModelForCausalLM.from_pretrained( 
+            model_name,
+            trust_remote_code=True,
+            device_map='auto',
+            torch_dtype=torch.bfloat16,
+            quantization_config=BitsAndBytesConfig(
+                load_in_4bit=True,
+                bnb_4bit_compute_dtype=torch.bfloat16,
+                bnb_4bit_use_double_quant=True,
+                bnb_4bit_quant_type='nf4'
+            ),
+        )
     else:
         # TODO: load the model without quantization
-        
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            trust_remote_code=True,
+            torch_dtype=torch.bfloat16
+        ).cuda()
 
     results = []
     results_processed = []
@@ -32,6 +47,19 @@ def prompt_model(dataset, model_name = "deepseek-ai/deepseek-coder-6.7b-base", q
         
         # TODO: prompt the model and get the response
 
+        inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+        
+        with torch.no_grad():
+            outputs = model.generate(
+                **inputs,
+                max_length=500,
+                do_sample=False
+            )
+        
+        response = tokenizer.decode(
+            outputs[0][inputs["input_ids"].shape[1]:],
+            skip_special_tokens=True
+        )
         
         print(f"Task_ID {case['task_id']}:\nPrompt:\n{prompt}\nResponse:\n{response}")
         results.append(dict(task_id=case["task_id"], completion=response))
